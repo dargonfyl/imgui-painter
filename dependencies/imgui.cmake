1452bdd597bf2b7a96d96ed5146c39730a6e36fa
dargonfyl <mian.wang@mail.utoronto.ca>
2020-12-08 13:47:10 -0700

    Bump imgui version

--- dependencies/imgui.cmake
+++ dependencies/imgui.cmake
@@ -5,13 +5,14 @@ add_library(
 	${CMAKE_CURRENT_LIST_DIR}/imgui/imgui_demo.cpp
 	${CMAKE_CURRENT_LIST_DIR}/imgui/imgui_draw.cpp
 	${CMAKE_CURRENT_LIST_DIR}/imgui/imgui_widgets.cpp
-	${CMAKE_CURRENT_LIST_DIR}/imgui/examples/imgui_impl_glfw.cpp
-	${CMAKE_CURRENT_LIST_DIR}/imgui/examples/imgui_impl_opengl3.cpp
-	${CMAKE_CURRENT_LIST_DIR}/imgui/examples/imgui_impl_opengl3.h  # Not sure if you need to include these here
-	${CMAKE_CURRENT_LIST_DIR}/imgui/examples/imgui_impl_glfw.h
+	${CMAKE_CURRENT_LIST_DIR}/imgui/imgui_tables.cpp
+	${CMAKE_CURRENT_LIST_DIR}/imgui/backends/imgui_impl_glfw.cpp
+	${CMAKE_CURRENT_LIST_DIR}/imgui/backends/imgui_impl_opengl3.cpp
+	${CMAKE_CURRENT_LIST_DIR}/imgui/backends/imgui_impl_opengl3.h  # Not sure if you need to include these here
+	${CMAKE_CURRENT_LIST_DIR}/imgui/backends/imgui_impl_glfw.h
 )
 target_include_directories(imgui PUBLIC ${CMAKE_CURRENT_LIST_DIR}/imgui)
-target_include_directories(imgui PUBLIC ${CMAKE_CURRENT_LIST_DIR}/imgui/examples)
+target_include_directories(imgui PUBLIC ${CMAKE_CURRENT_LIST_DIR}/imgui/backends)
 
 add_compile_definitions(IMGUI_IMPL_OPENGL_LOADER_GLAD)  # Sometimes, MSVC doesn't have correct preprocessor definitions
 
